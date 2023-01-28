8460182f1d49eb1a39ae159218485c99c9316ca6
tlsrhksgh <sin99145@naver.com>
2023-01-28 16:12:45 +0900

    Swagger 설정 적용

--- src/main/java/com/zero/weather/controller/DiaryController.java
+++ src/main/java/com/zero/weather/controller/DiaryController.java
@@ -2,6 +2,7 @@ package com.zero.weather.controller;
 
 import com.zero.weather.domain.Diary;
 import com.zero.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -15,17 +16,20 @@ import java.util.List;
 public class DiaryController {
     private final DiaryService diaryService;
 
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", notes = "일기 생성 api")
     @PostMapping("/create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                      @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
     List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
